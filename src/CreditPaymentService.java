001d811626c5bcf40dfd01cfc0211386545938cf
Anastasiya Riabova <vereten.o@yandex.ru>
2023-03-17 16:37:42 +0400

    add int

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public double calculate(double interestRate, int amountCredit, int creditTerm) {
+    public int calculate(double interestRate, int amountCredit, int creditTerm) {
 
         double rateMonth = interestRate / 100 / 12; // процентная ставка в месяц = ( разделите размер ставки на 100 и затем на 12 )
         double coefficient = (rateMonth * (Math.pow(1 + rateMonth, creditTerm))) / ((Math.pow(1 + rateMonth, creditTerm)) - 1); // коэффициент K = (r * (1 + r) ^ n) / (((1 + r) ^ n) - 1)
