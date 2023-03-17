536032df8d4ee83ed4e64757981cc48097f7e8fc
Anastasiya Riabova <vereten.o@yandex.ru>
2023-03-17 16:42:33 +0400

    add new int

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         // int amountCredit = 1_000_000; // сумма кредита
         // int creditTerm = 12 ; // срок кредита в месяцах
         int mouthPaymentOne = service.calculate(9.99, 1_000_000, 12); // ежемесячный платеж
-        int  mouthPaymentTwo = service.calculate(9.99, 1_000_000, 24); // ежемесячный платеж
-        int  mouthPaymentThree = service.calculate(9.99, 1_000_000, 36); // ежемесячный платеж
+        int mouthPaymentTwo = service.calculate(9.99, 1_000_000, 24); // ежемесячный платеж
+        int mouthPaymentThree = service.calculate(9.99, 1_000_000, 36); // ежемесячный платеж
 
         System.out.println("Ежемесячный платеж на 12 месяцев: " + mouthPaymentOne);
         System.out.println("Ежемесячный платеж на 24 месяца: " + mouthPaymentTwo);
